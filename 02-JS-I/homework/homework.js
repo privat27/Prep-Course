51dc899a436f404af5a8528390d6ce1ddd3e96a7
privat27 <videitos21@gmail.com>
2022-10-21 22:34:53 -0500

    cambios de prueba

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -1,13 +1,13 @@
 // En estas primeras 6 preguntas, reemplaza `null` por la respuesta
 
 // Crea una variable "string", puede contener lo que quieras:
-const nuevaString = 'hola'
+const nuevaString = 'ovidio'
 
 // Crea una variable numérica, puede ser cualquier número:
 const nuevoNum = 34
 
 // Crea una variable booleana:
-const nuevoBool = null
+const nuevoBool = true
 
 // Resuelve el siguiente problema matemático:
 const nuevaResta = 10 - null === 5
